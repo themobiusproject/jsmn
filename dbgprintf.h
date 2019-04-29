64d29f4cc1bab2e9069c8ff58cf875ba4a8d2fbc
Mark Conway <mark.conway@optcts.com>
2019-04-28 21:54:47 -0500

    dbgprintf update

--- dbgprintf.h
+++ dbgprintf.h
@@ -3,19 +3,34 @@
 #ifndef __DBGPRINTF_H
 #define __DBGPRINTF_H
 
-#if defined(NPRINTF)
+#if defined(NPRINTF) // don't print anything
 #define dbgprintf(...)
-#elif defined(NDEBUG)
+
+#else
+
+// OS specific print functions
+#if defined(__ANDROID__)
+#include <android/log.h>
+#define __dbgprintf(...) __android_log_print(ANDROID_LOG_DEBUG, "LOG_TAG", __VA_ARGS__);
+#else
+#define __dbgprintf(...) fprintf(stderr, __VA_ARGS__);
+#endif
+
+#if defined(NDEBUG)
+// print debug message
 #define dbgprintf(...) \
     do { \
-        fprintf(stderr, __VA_ARGS__); \
+        __dbgprintf(__VA_ARGS__); \
     } while (0)
 #else
+// print verbose debug message including file, line, and function
 #define dbgprintf(...) \
     do { \
-        fprintf(stderr, "%s : %d : %s()\n", __FILE__, __LINE__, __func__); \
-        fprintf(stderr, __VA_ARGS__); \
+        __dbgprintf("%s : %d : %s()\n", __FILE__, __LINE__, __func__); \
+        __dbgprintf(__VA_ARGS__); \
     } while (0)
-#endif
+#endif // NDEBUG
+
+#endif // NPRINTF
 
 #endif // __DBGPRINTF_H
